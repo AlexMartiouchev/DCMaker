c46474967e9ecdeb6392f18af176b7a5c1a4e032
AlexMartiouchev <alexmartiouchev@outlook.com>
2024-01-09 17:57:13 +0000

    updated depreciated model

--- populator/utils.py
+++ populator/utils.py
@@ -1,14 +1,16 @@
 from openai import OpenAI
+from django.conf import settings
 
 client = OpenAI(api_key=settings.OPENAI_API_KEY)
-from django.conf import settings
 
 def call_openai_api(prompt_text):
 
     try:
-        response = client.completions.create(model="text-davinci-003",  # Update the model name as per the new API
-        prompt=prompt_text,
-        max_tokens=150)
+        response = client.completions.create(
+            model="gpt-3.5-turbo-instruct",  # Update the model name as per the new API
+            prompt=prompt_text,
+            max_tokens=150
+        )
         return response.choices[0].text.strip()
     except Exception as e:
         # Handle any exceptions (e.g., API errors, network issues)
