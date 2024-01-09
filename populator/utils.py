50ce19efa53a3fd78160188b80388a8d75fc2628
AlexMartiouchev <alexmartiouchev@outlook.com>
2024-01-09 02:59:53 +0000

    updated openai

--- populator/utils.py
+++ populator/utils.py
@@ -1,15 +1,14 @@
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key=settings.OPENAI_API_KEY)
 from django.conf import settings
 
 def call_openai_api(prompt_text):
-    openai.api_key = settings.OPENAI_API_KEY
 
     try:
-        response = openai.Completion.create(
-            model="text-davinci-003",  # Update the model name as per the new API
-            prompt=prompt_text,
-            max_tokens=150
-        )
+        response = client.completions.create(model="text-davinci-003",  # Update the model name as per the new API
+        prompt=prompt_text,
+        max_tokens=150)
         return response.choices[0].text.strip()
     except Exception as e:
         # Handle any exceptions (e.g., API errors, network issues)
