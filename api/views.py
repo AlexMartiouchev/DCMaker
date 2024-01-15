8de752f6fb16e23e47d4edd37950077e7450b0d8
AlexMartiouchev <alexmartiouchev@outlook.com>
2024-01-15 00:24:48 +0000

    commented out unused structured_data, changed styling

--- api/views.py
+++ api/views.py
@@ -8,32 +8,38 @@ from populator.utils import call_openai_api, find_highest_faction_number, parse_
 
 def lore_maker_endpoint(request):
     if request.method == "POST":
-        structured_data = {
-            "location_fields": {
-                "locationType": request.POST.getlist("locationType"),
-                "locationPrompt": request.POST.getlist("locationPrompt"),
-            },
-        }
 
-        # Process factions
+        # keep structured_data for logging?
+
+        # structured_data = {
+        #     "location_fields": {
+        #         "locationType": request.POST.getlist("locationType"),
+        #         "locationPrompt": request.POST.getlist("locationPrompt"),
+        #     },
+        # }
+
+        # # Process factions
         faction_types = request.POST.getlist("factionType")
         faction_prompts = request.POST.getlist("factionPrompt")
 
-        for index in range(len(faction_types)):
-            structured_data[f"faction_{index+1}_fields"] = {
-                "factionType": faction_types[index],
-                "factionPrompt": faction_prompts[index],
-            }
+        # for index in range(len(faction_types)):
+        #     structured_data[f"faction_{index+1}_fields"] = {
+        #         "factionType": faction_types[index],
+        #         "factionPrompt": faction_prompts[index],
+        #     }
 
         # Process characters
         character_factions = request.POST.getlist("characterFactions")
         character_prompts = request.POST.getlist("characterPrompt")
+        party_number = request.POST.get("partyNumber", 4)
+        party_level = request.POST.get("partyLevel", 4)
 
-        for index in range(len(character_factions)):
-            structured_data[f"character_{index+1}_fields"] = {
-                "characterFactions": character_factions[index],
-                "characterPrompt": character_prompts[index],
-            }
+
+        # for index in range(len(character_factions)):
+        #     structured_data[f"character_{index+1}_fields"] = {
+        #         "characterFactions": character_factions[index],
+        #         "characterPrompt": character_prompts[index],
+        #     }
         
         # Send fields for location, receiving response as a full description and a summary
         location_prompt_text = PopulatorConfig.location_prompt + "\n\n"
