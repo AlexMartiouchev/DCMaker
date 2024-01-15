af7f4b7dacea18c3cce2d1f59b70b5a1917d1234
AlexMartiouchev <alexmartiouchev@outlook.com>
2024-01-15 00:33:19 +0000

    differentiated between lead and mob characters for prompts

--- populator/apps.py
+++ populator/apps.py
@@ -6,13 +6,16 @@ class PopulatorConfig(AppConfig):
     name = 'populator'
     location_prompt = ""
     faction_prompt = ""
-    character_prompt = ""
+    lead_character_prompt = ""
+    mob_character_prompt = ""
 
     def ready(self):
         with open('prompts/location_prompt.txt', 'r') as file:
             PopulatorConfig.location_prompt = file.read()
         with open('prompts/faction_prompt.txt', 'r') as file:
             PopulatorConfig.faction_prompt = file.read()
-        with open('prompts/character_prompt.txt', 'r') as file:
-            PopulatorConfig.character_prompt = file.read()
+        with open('prompts/lead_character_prompt.txt', 'r') as file:
+            PopulatorConfig.lead_character_prompt = file.read()
+        with open('prompts/mob_character_prompt.txt', 'r') as file:
+            PopulatorConfig.mob_character_prompt = file.read()
 
