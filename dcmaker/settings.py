002d944dd7d917ace63f993a248b74b4ea6d9863
Sasha <alexmartiouchev@outlook.com>
2026-07-17 18:54:05 +0100

    Remove legacy generation files and dead api app

--- dcmaker/settings.py
+++ dcmaker/settings.py
@@ -14,7 +14,6 @@ import os
 from pathlib import Path
 from decouple import config
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -35,7 +34,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "api.apps.ApiConfig",
     "populator.apps.PopulatorConfig",
     "users.apps.UsersConfig",
     "django.contrib.admin",
